3de9faf11354108346b38922686d5ff8d2eefc4c
SizableShrimp <jcharles08@me.com>
2018-07-24 22:23:06 -0500

    Add files via upload

---
+++ src/main/java/me/sizableshrimp/discordbot/WebController.java
@@ -0,0 +1,15 @@
+package me.sizableshrimp.discordbot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WebController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "The XT Discord Bot is currently running.";
+	}
+}
